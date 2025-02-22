02c85c95007789f73b8dba3405b117b347a88dea
ErfanBaeiri <ErfanBaeiri@gmail.com>
2025-02-22 20:55:41 +0330

    01.Transfer template to project
    Transfer template to project

--- SwiftStore.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SwiftStore.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
