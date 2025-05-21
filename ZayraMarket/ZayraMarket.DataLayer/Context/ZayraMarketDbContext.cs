f378235be6ad2be014040c99a8430e60ca000f9d
ErfanBaeiri <ErfanBaeiri@gmail.com>
2025-05-21 15:35:32 +0330

    Project implementation
    01.02.03.04:Initial implementation of onion architecture in the project
    05.Implementing Generic Repository

---
+++ ZayraMarket/ZayraMarket.DataLayer/Context/ZayraMarketDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZayraMarket.DataLayer.Entities.Account;
+
+namespace ZayraMarket.DataLayer.Context;
+
+public class ZayraMarketDbContext : DbContext
+{
+    #region dbsets
+    public DbSet<User> Users { get; set; }
+    #endregion
+}
---
+++ ZayraMarket/ZayraMarket.DataLayer/Entities/Account/User.cs
@@ -0,0 +1,14 @@
+﻿using ZayraMarket.DataLayer.Entities.Common;
+
+namespace ZayraMarket.DataLayer.Entities.Account;
+
+public class User : BaseEntity
+{
+    #region Properties
+
+    #endregion
+
+    #region Relations
+
+    #endregion
+}
